395b4cb5acc4a2ac70b3bc9e827db95f5c70fca2
Muhammad Adil <addhtml00@gmail.com>
2023-04-27 06:15:56 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import Avatar from "react-avatar-edit";
 function App() {
+  const [image, setImage] = useState("");
+  const onCrop = (view) => {
+    setImage(view);
+    console.log(view);
+  };
+
+  const onClose = (view) => {
+    setImage(null);
+  };
+
+  const onBeforeFileLoad = (elem) => {
+    console.log(elem.target.files[0]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Avatar
+        name="P1"
+        width={300}
+        height={300}
+        onCrop={onCrop}
+        label={"Select an Image"}
+        exportAsSquare={true}
+        onClose={onClose}
+        exportSize={800}
+        onBeforeFileLoad={onBeforeFileLoad}
+      />
+      {image && <img width={500} src={image} alt="dd" />}
     </div>
   );
 }
